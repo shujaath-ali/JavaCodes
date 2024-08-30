ac1d42896cbcd264c3c170d2f49843920d9dd5d6
Shujaath Ali <shujaath_ali@epam.com>
2024-08-30 08:12:43 +0530

    First commit

---
+++ LearningGit/src/main/java/com/shujaath/Controller/Product.java
@@ -0,0 +1,37 @@
+package com.shujaath.Controller;
+
+public class Product {
+    String name;
+    Long price;
+    String brand;
+
+    public Product(String name, Long price, String brand) {
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
--- LearningGit/src/main/java/com/shujaath/Main.java
+++ LearningGit/src/main/java/com/shujaath/Main.java
@@ -1,9 +1,22 @@
 package com.shujaath;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.shujaath.Controller.Product;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("We are practicing Hello world program");
+        List<Product> products = new ArrayList<>();
+
+        // Adding products to the list
+        products.add(new Product("Laptop", 1500L, "Dell"));
+        products.add(new Product("Smartphone", 800L, "Apple"));
+        products.add(new Product("Headphones", 150L, "Sony"));
+
+        // Iterating over the list to print only the names of the products
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
     }
 }
